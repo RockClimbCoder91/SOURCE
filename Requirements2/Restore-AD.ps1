daf4800838c5833869ccf712c339987aef4787df
Geno <rockclimbcoder91@protonmail.com>
2024-07-13 14:57:27 -0500

    updating ou script

--- Requirements2/Restore-AD.ps1
+++ Requirements2/Restore-AD.ps1
@@ -11,3 +11,22 @@ $allOUs | ForEach-Object {
     Write-Output "DistinguishedName: $($_.DistinguishedName)"
     Write-Output "--------------------"
 }
+
+
+# # Import the Active Directory module
+# Import-Module ActiveDirectory
+
+# # Define the domain components
+# $domainComponents = "DC=consultingfirm,DC=com"
+# $ouName = "Finance"
+
+# # Check if the OU exists
+# Write-Output "Checking for the existence of the Organizational Unit (OU) named '$ouName'..."
+# $ou = Get-ADOrganizationalUnit -Filter "Name -eq '$ouName'" -SearchBase $domainComponents -ErrorAction SilentlyContinue
+
+# if ($ou) {
+#     Write-Output "The Organizational Unit (OU) named '$ouName' exists."
+#     Write-Output "Distinguished Name: $($ou.DistinguishedName)"
+# } else {
+#     Write-Output "The Organizational Unit (OU) named '$ouName' does not exist."
+# }
