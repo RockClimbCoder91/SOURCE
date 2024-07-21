fed34d3efaac27764cb82c8fe77cefb0ac643865
Geno <rockclimbcoder91@protonmail.com>
2024-07-21 16:39:09 -0500

    added debugging for path

--- Requirements2/Restore-AD.ps1
+++ Requirements2/Restore-AD.ps1
@@ -7,7 +7,6 @@ Import-Module ActiveDirectory
 $ouName = "Finance"
 $domainComponents = "DC=consultingfirm,DC=com"
 $csvFilePath = Join-Path -Path $PSScriptRoot -ChildPath "financePersonnel.csv"
-$ouPath = "OU=$ouName,$domainComponents"
 
 # Function to remove all child objects within the OU
 function Remove-ChildObjects($ouPath) {
@@ -45,9 +44,10 @@ function Import-Users($csvFilePath, $ouPath) {
 
         # Create the user
         try {
+            Write-Output "Creating user in OU: $ouPath"
             New-ADUser -Name $displayName -GivenName $firstName -Surname $lastName -DisplayName $displayName `
                        -UserPrincipalName $userPrincipalName -SamAccountName $samAccountName `
-                       -Path "$ouPath" -PostalCode $postalCode -OfficePhone $officePhone `
+                       -Path $ouPath -PostalCode $postalCode -OfficePhone $officePhone `
                        -MobilePhone $mobilePhone -AccountPassword (ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force) `
                        -Enabled $true
             Write-Output "User '$displayName' has been created and added to the OU '$ouName'."
@@ -119,7 +119,7 @@ Import-Users -csvFilePath $csvFilePath -ouPath $ouPath
 
 # Generate the output file for submission
 try {
-    Get-ADUser -Filter * -SearchBase "$ouPath" -Properties DisplayName,PostalCode,OfficePhone,MobilePhone | 
+    Get-ADUser -Filter * -SearchBase $ouPath -Properties DisplayName,PostalCode,OfficePhone,MobilePhone | 
     Select-Object DisplayName,PostalCode,OfficePhone,MobilePhone | 
     Out-File -FilePath .\AdResults.txt
     Write-Output "Data from 'Client_A_Contacts' has been exported to 'AdResults.txt'."
